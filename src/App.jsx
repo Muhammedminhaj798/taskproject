d04875f5b7a0e418036a8c6987e283e6588f9c61
safvancentrric <safvan.centrric@gmail.com>
2025-01-11 16:55:46 +0530

    projecttask

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import Details from './details'
-import Home from './Home'
-import Context from './Context'
+import React, { useState } from 'react'
+
 
 function App() {
+  const [state, setState] = useState(0)
+
+
+
   return (
-    <Context>
-    <Routes>
-      <Route path='/details' element={<Details/>}/>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
-    </Context>
+    <div className='flex justify-center mt-64 gap-12'>
+      <div className={`${ state>= 1 ? "bg-yellow-500" : "bg-gray-300"} h-12 w-12 border `} onClick={()=>setState(1)}></div>
+      <div className={`${ state>= 2 ? "bg-yellow-500" : "bg-gray-300"}  h-12 w-12 border`} onClick={()=>setState(2)}></div>
+      <div className={`${ state>= 3 ? "bg-yellow-500" : "bg-gray-300"}  h-12 w-12 border`} onClick={()=>setState(3)}></div>
+      <div className={`${ state>= 4 ? "bg-yellow-500" : "bg-gray-300"}  h-12 w-12 border`}onClick={()=>setState(4)}></div>
+      <div className={`${ state>= 5 ? "bg-yellow-500" : "bg-gray-300"}  h-12 w-12 border`}onClick={()=>setState(5)}></div>
+    </div>
   )
 }
 
